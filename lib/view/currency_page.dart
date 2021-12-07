5145ffa4180731e229195b91e3793f8b20c8171c
Serhat ZENGİN <srhtzngn@hotmail.com>
2021-12-07 21:26:07 +0300

    Ürünlerde Düzeltmeler Yapıldı
    +Verilen Dummy ürünlerde düzeltmeler yapıldı.
    +Çekilen para birimleri EURO/DOLAR/TRY ye dönüştürüldü.

--- lib/view/currency_page.dart
+++ lib/view/currency_page.dart
@@ -1,70 +1,59 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:sigma_basket_app/model/api_model.dart';
 import 'package:sigma_basket_app/services/services.dart';
 
 class CurrencyPage extends StatefulWidget {
-  const CurrencyPage({Key? key}) : super(key: key);
+  const CurrencyPage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
-  State<CurrencyPage> createState() => _CurrencyPageState();
+  _CurrencyPageState createState() => _CurrencyPageState();
 }
 
 class _CurrencyPageState extends State<CurrencyPage> {
   @override
   Widget build(BuildContext context) {
-    var _service = HomeService(
-        Dio(BaseOptions(baseUrl: 'http://api.exchangeratesapi.io/v1')));
+    var _service = Currency().sendAndGet();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Currency Page"),
-      ),
-      body: Column(
-        children: [
-          Expanded(child: currencyApi(_service)),
-        ],
-      ),
-    );
-  }
-
-  FutureBuilder<ExchangeModel> currencyApi(HomeService _service) {
-    return FutureBuilder(
-      future: _service.fetchUsers(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            if (snapshot.data != null) {
-              ExchangeModel testmodel = snapshot.data as ExchangeModel;
-              return Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      child: Card(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text('EURO: ' + testmodel.rates.EUR.toString()),
-                            Text('USD: ' + testmodel.rates.USD.toString()),
-                            Text('TRY: ' + testmodel.rates.TRY.toString()),
-                          ],
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder(
+          future: _service,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                ExchangeModel model = snapshot.data as ExchangeModel;
+                var result = model.rates.USD * model.rates.EUR;
+                //işlermler
+                return Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        child: Card(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text('EURO: ' + model.rates.EUR.toString()),
+                              Text('USD: ' + model.rates.USD.toString()),
+                              Text('TRY: ' + model.rates.TRY.toString()),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          setState(() {});
-                        },
-                        child: const Text("Refresh"))
-                  ],
-                ),
-              );
+                      TextButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          child: const Text("Refresh"))
+                    ],
+                  ),
+                );
+              default:
+                return const Center(child: CircularProgressIndicator());
             }
-            return const Center(child: CircularProgressIndicator());
-          default:
-            return const Center(child: CircularProgressIndicator());
-        }
-      },
-    );
+          },
+        ));
   }
 }
