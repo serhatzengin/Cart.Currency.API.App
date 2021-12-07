7b92dd6b755421ba716455fbd3d933074ad059f4
Serhat ZENGİN <srhtzngn@hotmail.com>
2021-12-07 23:17:27 +0300

    Ürünlere Dolar Karşılığı Eklendi
    +Artık ürünlerin dolar karşılığına tek tuşla erişebilirsiniz !

--- lib/view/currency_page.dart
+++ lib/view/currency_page.dart
@@ -15,45 +15,48 @@ class _CurrencyPageState extends State<CurrencyPage> {
   Widget build(BuildContext context) {
     var _service = Currency().sendAndGet();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: FutureBuilder(
-          future: _service,
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                ExchangeModel model = snapshot.data as ExchangeModel;
-                var result = model.rates.USD * model.rates.EUR;
-                //işlermler
-                return Center(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 100,
-                        child: Card(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('EURO: ' + model.rates.EUR.toString()),
-                              Text('USD: ' + model.rates.USD.toString()),
-                              Text('TRY: ' + model.rates.TRY.toString()),
-                            ],
-                          ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: _service,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              ExchangeModel model = snapshot.data as ExchangeModel;
+              var result = model.rates.USD * model.rates.EUR;
+              //işlermler
+              return Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      child: Card(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text('EURO: ' + model.rates.EUR.toString()),
+                            Text(
+                                'USD: ' + model.rates.USD.toString() + " EURO"),
+                            Text(
+                                'TRY: ' + model.rates.TRY.toString() + " EURO"),
+                          ],
                         ),
                       ),
-                      TextButton(
-                          onPressed: () {
-                            setState(() {});
-                          },
-                          child: const Text("Refresh"))
-                    ],
-                  ),
-                );
-              default:
-                return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ));
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        child: const Text("Refresh"))
+                  ],
+                ),
+              );
+            default:
+              return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
